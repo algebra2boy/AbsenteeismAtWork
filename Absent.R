9262ab345a731fc506ffcb480cae006af553a176
Yongye Tan <yongyetan1209@gmail.com>
2022-12-12 18:04:52 -0500

    code completed

--- Absent.R
+++ Absent.R
@@ -28,7 +28,7 @@ aggregate(data.frame(count = Education), list(value = Education), length)
 #  4     4     4 # master and doctor
 
 
-
+# Figure 1
 # exploring the relationship between the age and amount of the work in a day
 absent %>% 
   # group the age group
@@ -57,14 +57,17 @@ absent %>%
 ggsave("age_workload.pdf")
 
 
-
+# Figure 2
+# exploring the relationship between the age and absent time
 absent %>% 
   group_by(Age) %>% 
   summarise(average_absent = mean(Absenteeism.time.in.hours)) %>% 
   ggplot(aes(x = Age,  y = average_absent)) + 
   geom_point(aes(color = Age)) + 
   geom_smooth(se = FALSE, color = "green", method = 'loess', formula = 'y ~ x') + 
+  # customize the x-tick from 27 to 57 with an interval of 3
   scale_x_continuous(breaks = seq(27, 57, by = 3)) + 
+  # customize the y-tick from 0 to 33 with an interval of 5
   scale_y_continuous(breaks = seq(0, 33, by = 5)) + 
   labs(x = "age of worker",
        y = "average absent time per week in hours",
@@ -73,7 +76,7 @@ absent %>%
 ggsave("absentTime_age.pdf")
   
   
-
+# Figure 3
 # making a graph about the number average absence across different workdays
 absent %>% group_by(Day.of.the.week) %>% 
   summarise(total_absence = mean(Reason.for.absence)) %>% 
@@ -86,6 +89,7 @@ absent %>% group_by(Day.of.the.week) %>%
     "Thursday" = '5', "Friday" = '6')) %>% 
   ggplot(aes(x = Day.of.the.week, y = total_absence)) + 
   geom_bar(stat = "identity") + 
+  # adding labels 
   labs(x = "Day of a week",
        y = "the number of times of absence in average",
        title = "Average absence vs weekday",
@@ -97,10 +101,16 @@ absent %>% group_by(Day.of.the.week) %>%
   coord_flip()
 ggsave("absent_workday.pdf")
 
+
+# Figure 4
+# making a graph about the number average absence across different months
 absent %>% group_by(Month.of.absence) %>% 
   summarise(Reason.for.absence = mean(Reason.for.absence)) %>% 
+  # get rid of the first row because 0 month is meaningless
   slice(2:13) %>% 
+  # convert it to charater array because recode needs an character vector
   mutate(Month.of.absence = as.character(Month.of.absence)) %>% 
+  # refactor the months
   mutate(Month.of.absence = fct_recode(
     Month.of.absence, "January" = '1', "February" = '2',
     "March" = '3', "April" = '4',
@@ -109,6 +119,7 @@ absent %>% group_by(Month.of.absence) %>%
     "September" = '9', "October" = "10",
     "November" = "11", "December" = "12"
   )) %>% 
+  # sort the absence time 
   ggplot(mapping = aes(Reason.for.absence, fct_reorder(Month.of.absence, Reason.for.absence, .fun = mean))) + 
   geom_point(colour = "red") + 
   labs(y = "Month of a year",
@@ -119,33 +130,44 @@ ggsave("absent_month.pdf")
 
 
 # grouping factor must have exactly 2 levels 
+# this is where I perform my t-test to answer 
+# Does bad habits lead to not showing up to work?
 t.test(Absenteeism.time.in.hours~Social.smoker, data = absent)
 t.test(Absenteeism.time.in.hours~Social.drinker, data = absent)
 t.test(Absenteeism.time.in.hours~Disciplinary.failure, data = absent)
 
-
+# label the distance with more meaningful labels
+# explore the relationship between distance and average absent time
+# Figure 5
 refactor_absent <- absent %>% 
   mutate(Distance.from.Residence.to.Work =
            case_when( (Distance.from.Residence.to.Work>0) & (Distance.from.Residence.to.Work <= 10) ~ "0~10", 
                       (Distance.from.Residence.to.Work>10) & (Distance.from.Residence.to.Work <= 20) ~ "10~20", 
                      (Distance.from.Residence.to.Work>20) & (Distance.from.Residence.to.Work <= 30) ~ "20~30",
                     (Distance.from.Residence.to.Work>30) & (Distance.from.Residence.to.Work <= 40) ~ "30~40",
-                    (Distance.from.Residence.to.Work>40 & Distance.from.Residence.to.Work<=50)  ~ "40~55",
-                    Distance.from.Residence.to.Work>= 50 ~ "55+")) %>% 
+                    (Distance.from.Residence.to.Work>40 & Distance.from.Residence.to.Work<=50)  ~ "40~50",
+                    Distance.from.Residence.to.Work> 50 ~ "50+")) %>% 
   group_by(Distance.from.Residence.to.Work) %>% 
   summarise(average_absent = mean(Absenteeism.time.in.hours))
 
 Distance_label <- refactor_absent$Distance.from.Residence.to.Work
 Distance_average <- refactor_absent$average_absent
 
+# find the percentage of the average distance
 percentage = round(Distance_average/sum(Distance_average) * 100)
 new_labels = paste(Distance_label, "-", percentage, "%", sep="")
+
+# creating the pie chart 
 pie(Distance_average, labels = new_labels, 
-    main = "Distance from residence to work in kilometer", 
+    # the title of the graph
+    main = "distance from residence to work in kilometer", 
+    # adding 6 rainbow colors 
     col = rainbow(6),
     angle = 30, 
+    # the size of circle
     radius = 0.9,
     lwd = 2)
 
+# 3D pie-chart, not used in this paper 
 pie3D(Distance_average, labels = new_labels, main = "Distance from residence to work in kilometer", col = rainbow(6), explode = 0.1)
 
